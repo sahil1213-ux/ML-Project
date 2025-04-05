bd6338c0cd352044e8a281a3db36bea746a05674
sahil1213-ux <sahilanand00027@gmail.com>
2025-04-05 18:34:18 +0530

    "Data Ingestion"

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,3 +1,51 @@
 # this file involves reading the data from the csv file and converting it into a pandas dataframe
 # and then converting it into a numpy array
 # and then converting it into a list and dividing it into train and test data
+
+# now all eda code should be done in this components folder to have CI/CD implemented
+
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join('artifacts', 'train.csv')
+    test_data_path: str = os.path.join('artifacts', 'test.csv')
+    raw_data_path: str = os.path.join('artifacts', 'data.csv')
+
+class DataIngestion:
+    def __init__(self):
+        self.config = DataIngestionConfig()
+    
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+            os.makedirs(os.path.dirname(self.config.train_data_path), exist_ok=True)
+            df.to_csv(self.config.raw_data_path, index=False, header=True)
+
+            logging.info('Train test split initiated')
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            train_set.to_csv(self.config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.config.test_data_path, index=False, header=True)
+
+            logging.info('Ingestion of data is completed')
+            return (
+                self.config.train_data_path,
+                self.config.test_data_path,
+                self.config.raw_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys) from e
+        
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
