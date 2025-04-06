0f015d5669b4217ad1865c299e3a631ab74fe0c4
sahil1213-ux <sahilanand00027@gmail.com>
2025-04-06 18:39:26 +0530

    "Prediction pipeline implementated and tested in Python"

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill
+# import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
@@ -53,6 +53,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+    # going to the file path using rb and loading the pickle file
 def load_object(file_path):
     try:
         with open(file_path, "rb") as file_obj:
